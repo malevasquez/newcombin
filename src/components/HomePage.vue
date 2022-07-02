aeaf214affb12aa16e08c320d389cb9715f7e66c
Malena Vasquez <male.vasquez@yahoo.com.ar>
2022-07-02 01:56:17 -0300

    Initial files + header (with tabs)

---
+++ src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <PageHeader/>
+</template>
+
+<script>
+  import PageHeader from "@/components/PageHeader";
+  export default {
+    name: 'HomePage',
+    components: {PageHeader},
+    data: () => ({
+    }),
+  }
+</script>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <v-tabs
+        background-color="#009A48"
+        color="black"
+        grow
+        height="72px"
+    >
+      <v-tabs-slider color="black"></v-tabs-slider>
+      <v-tab
+          v-for="tab in tabs"
+          :key="tab"
+      >
+        {{ tab }}
+      </v-tab>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  data () {
+    return {
+      tabs: [
+        "Home", "Other Page"
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.v-tab {
+  text-transform: none !important;
+}
+</style>
